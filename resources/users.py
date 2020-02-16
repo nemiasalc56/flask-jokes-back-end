a4aa046149a1f329f155624d24da46be2e86a81b
Nehemias Alcantara <nemiasalc56@gmail.com>
2020-02-15 20:56:52 -0600

    checks if a user already exist

--- resources/users.py
+++ resources/users.py
@@ -1,5 +1,6 @@
 import models
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from flask_bcrypt import generate_password_hash
 
 
 # make this a blueprint
@@ -18,4 +19,26 @@ def register():
 	payload = request.get_json()
 	print(payload)
 
-	return "register route works"
+	# make username and email lowercase
+	payload['username'] = payload['username'].lower()
+	payload['email'] = payload['email'].lower()
+
+	try:
+		# check if the user already exists
+		# if they do, we won't create the user
+		models.User.get(models.User.email == payload['email'])
+		# if the query doesn't cause an error, then the user exists
+		print(payload['email'])
+
+		return jsonify(
+			data={},
+			message="A user with that email already exists",
+			status=401
+			), 401
+
+	# if we get the error, the user does not exist
+	# so can proceed
+	except models.DoesNotExist:
+		
+
+		return "user does not exist"
